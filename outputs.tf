7eb40d0f42b51253cde7589c8914639f3ba802a5
Jas Tandon <jas.mostwanted@icloud.com>
2025-09-16 13:52:47 -0400

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -8,12 +8,22 @@ output "composite_alarm_arns" {
   value       = { for k, a in aws_cloudwatch_composite_alarm.this : k => a.arn }
 }
 
-output "dashboard_names" {
-  description = "List of dashboard names created"
-  value       = keys(aws_cloudwatch_dashboard.this)
-}
-
 output "sns_topic_arn" {
   description = "SNS topic ARN if created, else null"
   value       = var.create_sns_topic ? aws_sns_topic.this[0].arn : null
+}
+
+output "dashboard_names" {
+  description = "Names of created dashboards."
+  value       = [for k, _ in aws_cloudwatch_dashboard.this : k]
+}
+
+output "alarm_events_log_group_arn" {
+  description = "Log group ARN receiving Alarm State Change events (null if disabled)."
+  value       = local._log_group_arn
+}
+
+output "alarm_state_change_rule_arn" {
+  description = "EventBridge rule ARN (null if disabled)."
+  value       = try(aws_cloudwatch_event_rule.alarm_state_change[0].arn, null)
 }
